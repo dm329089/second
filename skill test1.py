108a9974c8423dc4b9d9705fe5b60c15144ccfa1
dm329089 <110012477+dm329089@users.noreply.github.com>
2022-07-25 23:32:45 -0700

    Add files via upload

---
+++ skill test1.py
@@ -0,0 +1,17 @@
+def binarysearch(list,key):
+    low=0
+    high=0
+    count=0
+    for i in range(len(a)):
+        if(key==list(i)):
+            mid=mid+1
+            return
+        else:
+          found==0
+        print("the key element is not found ")
+
+
+list=[12,24,36,48,65]
+print("the original array")
+key=int(input("enter the key element"))
+binarysearch(list.key)
